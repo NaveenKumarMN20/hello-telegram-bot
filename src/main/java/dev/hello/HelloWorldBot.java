57e0b81d45972c484215f7c6858fc37d0bfa07dc
NaveenKumarMN20 <mnnk202@gmail.com>
2021-03-28 13:28:23 +0530

    Added logging and cleaned up packages.

--- src/main/java/dev/hello/HelloWorldBot.java
+++ src/main/java/dev/hello/HelloWorldBot.java
@@ -1,19 +1,24 @@
 package dev.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class HelloWorldBot extends TelegramLongPollingBot {
+
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldBot.class);
+
     @Override
     public void onUpdateReceived(Update update) {
 
         if (update.getMessage().getText().equals("/hello")) {
+            log.info("/hello message from user id {} && user name is {}",update.getMessage().getFrom().getId(),update.getMessage().getFrom().getUserName());
             try {
                 SendMessage sendMessage = new SendMessage();
                 sendMessage.setText("Hello " + update.getMessage().getFrom().getUserName());
-                System.err.println(update.getMessage().getChatId());
                 sendMessage.setChatId(update.getMessage().getChatId().toString());
                 execute(sendMessage);
             } catch (TelegramApiException e) {
